a263a2974bdcb366402a8755f53ff3b869722bab
Keurys <77747734+keurysk@users.noreply.github.com>
2021-04-23 16:12:58 -0400

    Update Template.js

--- Template.js
+++ Template.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const { success, error } = require('../../config/default.json');
+const { success, error } = require('./../config/default.json');
 
 module.exports = {
 	name: '',  
